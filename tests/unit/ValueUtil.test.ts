63277c37af649ed9365818748bd3f57e4a372750
code-sunbo <bb.sunbo@huawei.com>
2024-06-06 14:05:20 +0800

    Fixed unit test build error.
    Signed-off-by: code-sunbo <bb.sunbo@huawei.com>

--- tests/unit/ValueUtil.test.ts
+++ tests/unit/ValueUtil.test.ts
@@ -13,30 +13,33 @@
  * limitations under the License.
  */
 
-import { Constant } from "../../src/core/base/Constant";
 import { NullType, NumberType, StringType, UndefinedType } from "../../src/core/base/Type";
 import { ValueUtil } from "../../src/core/common/ValueUtil";
-import { assert, describe, expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 
 describe("ValueUtil Test", () => {
     it('string case', () => {
         let type = StringType.getInstance();
-        expect(ValueUtil.getDefaultInstance(type))
-            .toEqual(ValueUtil.getStringTypeDefaultValue());
+        let value = ValueUtil.createStringConst('');
+        expect(value.getType())
+            .toEqual(type);
     })
     it('normal case 2', () => {
         let type = NumberType.getInstance();
-        expect(ValueUtil.getDefaultInstance(type))
-            .toEqual(ValueUtil.getNumberTypeDefaultValue());
+        let value = ValueUtil.getOrCreateNumberConst(0);
+        expect(value.getType())
+            .toEqual(type);
     })
     it('normal case 3', () => {
         let type = UndefinedType.getInstance();
-        expect(ValueUtil.getDefaultInstance(type))
-            .toEqual(ValueUtil.getUndefinedTypeDefaultValue());
+        let value = ValueUtil.getUndefinedConst();
+        expect(value.getType())
+            .toEqual(type);
     })
     it('normal case 4', () => {
         let type = NullType.getInstance();
-        expect(ValueUtil.getDefaultInstance(type))
-            .toEqual(new Constant('null', type));
+        let value = ValueUtil.getNullConstant();
+        expect(value.getType())
+            .toEqual(type);
     })
 })
