71a6ca94af2856e9bb09c8012952e0ace821d836
huang jianhua <huangjianhua13@huawei.com>
2024-06-20 17:09:48 +0800

    bugfix for type of base in ArkInstanceInvokeExpr
    Signed-off-by: huang jianhua <huangjianhua13@huawei.com>

--- tests/resources/arkIRTransformer/mainModule/main.ts
+++ tests/resources/arkIRTransformer/mainModule/main.ts
@@ -125,23 +125,23 @@
 // global as size
 // let sz3 = 3;
 
-function main() {
-    // let sz1 = 1;
-    // let arr1 = new Array(sz1);
-    // let arr2 = new Array(2);
-    // let arr3 = new Array(sz3);
-    //
-    // let arr4 = [1, 2, 3];
-    // let arr5 = [];
-    //
-    // let arr6 = new Array();
-    //
-    // let arr7 = new Array(1, 2, 3);
-    // let arr8 = [1, 2, '3'];
-
-    let arr9:number[] = [];
-    let arr10:any[] = [];
-}
+// function main() {
+//     // let sz1 = 1;
+//     // let arr1 = new Array(sz1);
+//     // let arr2 = new Array(2);
+//     // let arr3 = new Array(sz3);
+//     //
+//     // let arr4 = [1, 2, 3];
+//     // let arr5 = [];
+//     //
+//     // let arr6 = new Array();
+//     //
+//     // let arr7 = new Array(1, 2, 3);
+//     // let arr8 = [1, 2, '3'];
+//
+//     let arr9:number[] = [];
+//     let arr10:any[] = [];
+// }
 
 // // ConditionalExpression
 // function main() {
@@ -172,3 +172,8 @@ function main() {
 //     let b = -a + 1;
 // }
 
+function main() {
+    let i = 'hi'.charAt(0);
+
+    let j = /hjh/.test(i);
+}
