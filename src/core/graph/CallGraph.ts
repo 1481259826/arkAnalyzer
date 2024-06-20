f405acab345d7f37b21ce45cbd0bb6959a8791f6
Xu-Lingyun <xulingyun1@huawei.com>
2024-06-20 15:30:43 +0800

    pta refactoring: add graph data structure
    Signed-off-by: Xu-Lingyun <xulingyun1@huawei.com>

--- src/core/graph/CallGraph.ts
+++ src/core/graph/CallGraph.ts
@@ -1,25 +1,39 @@
-import { BaseEdge, BaseGraph, BaseNode } from './BaseGraph';
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { NodeID, Kind, BaseEdge, BaseGraph, BaseNode } from './BaseGraph';
 import { MethodSignature } from '../model/ArkSignature'
 import { Stmt, ArkInvokeStmt } from '../base/Stmt'
 
-type Method = MethodSignature;
+export type Method = MethodSignature;
 type StmtSet = Set<Stmt>;
-type CallSiteID = number;
-type CallSite = [CallSiteID, Stmt];
+export type CallSiteID = number;
+export type CallSite = [Stmt, Method];
 
-class CallEdge extends BaseEdge {
+export class CallGraphEdge extends BaseEdge {
     private directCalls: StmtSet;
     private indirectCalls: StmtSet;
     private callSiteID: CallSiteID;
 
-    constructor(src: CallNode, dst: CallNode, kind: number, csID: CallSiteID) {
+    constructor(src: CallGraphNode, dst: CallGraphNode, kind: number, csID: CallSiteID) {
         super(src, dst, kind);
         this.callSiteID = csID;
     }
-
 }
 
-class CallNode extends BaseNode {
+export class CallGraphNode extends BaseNode {
     private method: Method;
 
     constructor(id: number, m: Method) {
@@ -32,10 +46,52 @@ class CallNode extends BaseNode {
     }
 }
 
-class CallGraph extends BaseGraph {
+export class CallGraph extends BaseGraph {
     private idToCallSiteMap: Map<CallSiteID, CallSite>;
-    private nodeNum: number;
+    private callSiteToIdMap: Map<CallSite, CallSiteID>;
+    private methodToCGNodeMap: Map<Method, CallGraphNode>;
+    private nodeNum: number = 0;
+    private callSiteNum: number = 0;
     private directCallEdgeNum: number;
     private inDirectCallEdgeNum: number;
 
+    constructor() {
+        super();
+    }
+
+    public addCallGraphNode(method: Method): void {
+        let id: NodeID = this.nodeNum++;
+        let cgNode = new CallGraphNode(id, method);
+        this.addNode(cgNode);
+        this.methodToCGNodeMap.set(method, cgNode);
+    }
+
+    public getCallGraphNodeByMethod(method: Method): CallGraphNode | undefined {
+        let n = this.methodToCGNodeMap.get(method);
+        if (n == undefined) {
+            throw(new Error("CallGraphNode can't be found for method " + method.toString()))
+        }
+
+        return n;
+    }
+
+    public addDirectCallEdge(caller: Method, callee: Method, callStmt: Stmt): void {
+        let callerNode = this.getCallGraphNodeByMethod(caller) as CallGraphNode;
+        let calleeNode = this.getCallGraphNodeByMethod(callee) as CallGraphNode;
+
+        let cs: CallSite = [callStmt, callee];
+        let csID: CallSiteID;
+        if (!this.callSiteToIdMap.has(cs)) {
+            csID = this.callSiteNum++;
+            this.idToCallSiteMap.set(csID, cs);
+            this.callSiteToIdMap.set(cs, csID);
+        } else {
+            csID = this.callSiteToIdMap.get(cs) as CallSiteID;
+        }
+
+        // TODO: check if edge exists 
+        let callEdge = new CallGraphEdge(callerNode, calleeNode, 0, csID);
+        callEdge.getSrcNode().addOutgoingEdge(callEdge);
+        callEdge.getDstNode().addIncomingEdge(callEdge);
+    }
 }
