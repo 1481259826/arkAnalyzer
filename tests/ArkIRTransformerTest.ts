1e8100429077c3965c7925e4a1e01dae522261f9
huang jianhua <huangjianhua13@huawei.com>
2024-06-24 11:24:20 +0800

    add unittest for ArkIRTransformer
    Signed-off-by: huang jianhua <huangjianhua13@huawei.com>

--- tests/ArkIRTransformerTest.ts
+++ tests/ArkIRTransformerTest.ts
@@ -56,8 +56,9 @@ class ArkIRTransformerTest {
     public testStmtsOfSimpleProject() {
         logger.error('testStmtsOfSimpleProject start');
 
-        const projectDir = 'tests/resources/ArkIRTransformer/mainModuleEts';
+        // const projectDir = 'tests/resources/ArkIRTransformer/mainModuleEts';
         // const projectDir = 'tests/resources/arkIRTransformer/mainModule';
+        const projectDir = 'tests/resources/arkIRTransformer/expression';
         const sceneConfig: SceneConfig = new SceneConfig();
         sceneConfig.buildFromProjectDir(projectDir);
 
@@ -97,18 +98,18 @@ class ArkIRTransformerTest {
         const cfg = body.getCfg();
         for (const threeAddresStmt of cfg.getStmts()) {
             logger.error(`text: ${threeAddresStmt.toString()}`);
-            logger.error(`-original position: ${threeAddresStmt.getOriginPositionInfo().getLineNo()}, ${threeAddresStmt.getOriginPositionInfo().getColNo()}`);
+            // logger.error(`-original position: ${threeAddresStmt.getOriginPositionInfo().getLineNo()}, ${threeAddresStmt.getOriginPositionInfo().getColNo()}`);
             // if (threeAddresStmt.getOriginPositionInfo().getLineNo() === -1) {
             //     logger.error(`text: ${threeAddresStmt.toString()}`);
             // }
         }
 
-        logger.error('--- originalStmts ---');
-        const originalCfg = body.getOriginalCfg();
-        for (const originalStmt of originalCfg.getStmts()) {
-            logger.error(`text: ${originalStmt.toString()}`);
-            logger.error(`-original position: ${originalStmt.getOriginPositionInfo().getLineNo()}, ${originalStmt.getOriginPositionInfo().getColNo()}`);
-        }
+        // logger.error('--- originalStmts ---');
+        // const originalCfg = body.getOriginalCfg();
+        // for (const originalStmt of originalCfg.getStmts()) {
+        //     logger.error(`text: ${originalStmt.toString()}`);
+        //     logger.error(`-original position: ${originalStmt.getOriginPositionInfo().getLineNo()}, ${originalStmt.getOriginPositionInfo().getColNo()}`);
+        // }
     }
 
     private printScene(scene: Scene): void {
@@ -133,5 +134,5 @@ class ArkIRTransformerTest {
 
 const arkIRTransformerTest = new ArkIRTransformerTest();
 // arkIRTransformerTest.testSimpleStmt();
-// arkIRTransformerTest.testStmtsOfSimpleProject();
-arkIRTransformerTest.testStmtsOfEtsProject();
+arkIRTransformerTest.testStmtsOfSimpleProject();
+// arkIRTransformerTest.testStmtsOfEtsProject();
