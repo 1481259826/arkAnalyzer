cc20e0281f3a2f0b9c99506107a27b8e3b4468f0
Ji Xuye <jixuye@huawei.com>
2024-06-29 17:20:59 +0800

    infer type refactor
    Signed-off-by: Ji Xuye <jixuye@huawei.com>

--- tests/unit/save/SourceModules.test.ts
+++ tests/unit/save/SourceModules.test.ts
@@ -13,8 +13,8 @@
  * limitations under the License.
  */
 
-import { SceneConfig, Scene, SourceFilePrinter } from '../../../src/index';
-import { assert, describe, expect, it } from 'vitest';
+import { Scene, SceneConfig, SourceFilePrinter } from '../../../src/index';
+import { describe, expect, it } from 'vitest';
 import path from 'path';
 
 const CASE1_EXPECT = `import {ZipCodeValidator} from './classes';
@@ -27,8 +27,8 @@ import * as validator3.mainValidator from './classes';
 import * as validator3.ParseIntBasedZipCodeValidator from './classes';
 import validator4 from './classes';
 let myValidator1 = new ZipCodeValidator();
-let myValidator2 = new ZCV();
-let myValidator3 = new validator3.ZipCodeValidator();
+let myValidator2 = new ZipCodeValidator();
+let myValidator3 = new ZipCodeValidator();
 let myValidator4 = new validator4();
 export {ZipCodeValidator as RegExpBasedZipCodeValidator} from './classes';
 export * from './classes';
