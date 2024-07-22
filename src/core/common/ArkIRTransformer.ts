6b96404d15a5a9fb4fc30a91add09d1e839946e9
Xu-Lingyun <xulingyun1@huawei.com>
2024-07-22 14:30:59 +0800

    revert changes on IRTransformer.ts
    Signed-off-by: Xu-Lingyun <xulingyun1@huawei.com>

--- src/core/common/ArkIRTransformer.ts
+++ src/core/common/ArkIRTransformer.ts
@@ -143,9 +143,6 @@ export class ArkIRTransformer {
 
     public prebuildStmts(): Stmt[] {
         const stmts: Stmt[] = [];
-        const thisRef = new ArkThisRef(this.getThisLocal().getType() as ClassType);
-        stmts.push(new ArkAssignStmt(this.getThisLocal(), thisRef));
-
         let index = 0;
         for (const methodParameter of this.declaringMethod.getParameters()) {
             const parameterRef = new ArkParameterRef(index, methodParameter.getType());
@@ -153,6 +150,8 @@ export class ArkIRTransformer {
             index++;
         }
 
+        const thisRef = new ArkThisRef(this.getThisLocal().getType() as ClassType);
+        stmts.push(new ArkAssignStmt(this.getThisLocal(), thisRef));
         return stmts;
     }
 
