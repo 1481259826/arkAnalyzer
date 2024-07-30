ea01f0c1c81c5527dfb567b926236b5b36860a61
lqi27 <lqi27@163.com>
2024-07-30 17:16:44 +0800

    Fix fileutils bug & remove unused method
    Signed-off-by: lqi27 <lqi27@163.com>

--- src/utils/getAllFiles.ts
+++ src/utils/getAllFiles.ts
@@ -23,6 +23,7 @@ const logger = Logger.getLogger();
  * @param srcPath string 要提取文件的项目入口，相对或绝对路径都可
  * @param exts string[] 要提取的文件扩展名数组，每个扩展名需以点开头
  * @param filenameArr string[] 用来存放提取出的文件的原始路径的数组，可不传，默认为空数组
+ * @param visited: Set<string> 用来存放已经访问过的路径，避免递归栈溢出，可不传，默认为空数组
  * @return string[] 提取出的文件的原始路径数组
  */
 export function getAllFiles(
