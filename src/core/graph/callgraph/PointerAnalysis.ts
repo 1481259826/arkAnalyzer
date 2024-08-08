31b411e99669e45a37db4f4485630631d5376aa1
Xu-Lingyun <xulingyun1@huawei.com>
2024-08-08 17:02:40 +0800

    change the dynamic call set to map to let each pta itorate time can get dynamic callsite
    Signed-off-by: Xu-Lingyun <xulingyun1@huawei.com>

--- src/core/graph/callgraph/PointerAnalysis.ts
+++ src/core/graph/callgraph/PointerAnalysis.ts
@@ -402,7 +402,7 @@ export class PointerAnalysis extends AbstractAnalysis{
             return;
         }
 
-        let findSameType = true;
+        let findSameType = false;
         let pts = node.getPointTo();
         pts.forEach(pt => {
             let ptNode = this.pag.getNode(pt) as PagNode;
