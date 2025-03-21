1da4a4832f4180b3b1efe66fa550b9a86d0fb999
sfoolish <lqi27@163.com>
2025-03-21 21:48:19 +0800

    mv AppTest.ts to samples
    Signed-off-by: sfoolish <lqi27@163.com>

--- tests/samples/AppTest.ts
+++ tests/samples/AppTest.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,17 +13,22 @@
  * limitations under the License.
  */
 
-import { SceneConfig } from '../src/Config';
-import { Scene } from '../src/Scene';
+import { SceneConfig } from '../../src';
+import { Scene } from '../../src';
+import { Logger, LOG_LEVEL, LOG_MODULE_TYPE } from '../../src';
+
+const logger = Logger.getLogger(LOG_MODULE_TYPE.TOOL, 'APPTEST');
+Logger.configure('', LOG_LEVEL.ERROR, LOG_LEVEL.INFO, false);
 
 let config: SceneConfig = new SceneConfig();
 
 // build from json
-config.buildFromJson("./tests/AppTestConfig.json");
+config.buildFromJson("./tests/samples/AppTestConfig.json");
 function runScene4Json(config: SceneConfig) {
     let projectScene: Scene = new Scene();
     projectScene.buildBasicInfo(config);
     projectScene.buildScene4HarmonyProject();
     projectScene.inferTypes();
+    logger.info('runScene4Json exit.')
 }
 runScene4Json(config);
