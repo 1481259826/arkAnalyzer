42b373226d0d15dd3f3445d62144ef6a73c01c57
sfoolish <lqi27@163.com>
2025-03-24 20:40:46 +0800

    mv UndefinedVariableTest.ts to samples dir
    Signed-off-by: sfoolish <lqi27@163.com>

--- tests/samples/UndefinedVariableTest.ts
+++ tests/samples/UndefinedVariableTest.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,26 +13,30 @@
  * limitations under the License.
  */
 
-import { SceneConfig } from '../src/Config';
-import { Scene } from '../src/Scene';
-import { ModelUtils } from '../src/core/common/ModelUtils';
-import { UndefinedVariableChecker, UndefinedVariableSolver } from '../src/core/dataflow/UndefinedVariable';
+import { SceneConfig } from '../../src';
+import { Scene } from '../../src';
+import { ModelUtils } from '../../src';
+import { UndefinedVariableChecker, UndefinedVariableSolver } from '../../src';
+import { Logger, LOG_LEVEL, LOG_MODULE_TYPE } from '../../src';
 
-// const config_path = "tests\\resources\\ifds\\UndefinedVariable\\ifdsTestConfig.json";
+const logger = Logger.getLogger(LOG_MODULE_TYPE.TOOL, 'UndefinedVariableTest');
+Logger.configure('', LOG_LEVEL.ERROR, LOG_LEVEL.INFO, false);
+
+
 let config: SceneConfig = new SceneConfig();
 config.buildFromProjectDir("tests/resources/ifds/UndefinedVariable");
 const scene = new Scene();
 scene.buildBasicInfo(config);
 scene.buildSceneFromProjectDir(config);
 const defaultMethod = scene.getFiles()[0].getDefaultClass().getDefaultArkMethod();
-let method = ModelUtils.getMethodWithName("U4",defaultMethod!);
+let method = ModelUtils.getMethodWithName("u4",defaultMethod!);
 if(method){
     const problem = new UndefinedVariableChecker([...method.getCfg()!.getBlocks()][0].getStmts()[method.getParameters().length],method);
     const solver = new UndefinedVariableSolver(problem, scene);
     solver.solve();
     for (const outcome of problem.getOutcomes()) {
         let position = outcome.stmt.getOriginPositionInfo();
-        console.log('undefined error in line ' + position.getLineNo());
+        logger.info('undefined error in line ' + position.getLineNo());
     }
     debugger;
 }
