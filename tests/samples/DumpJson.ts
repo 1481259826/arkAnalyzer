a07b6f3ed57e8fca112058d30d5a21111934b17d
sfoolish <lqi27@163.com>
2025-03-24 19:28:26 +0800

    mv DumpJson.ts to samples dir
    Signed-off-by: sfoolish <lqi27@163.com>

--- tests/samples/DumpJson.ts
+++ tests/samples/DumpJson.ts
@@ -13,16 +13,16 @@
  * limitations under the License.
  */
 
-import { SceneConfig } from '../src/Config';
-import { PrinterBuilder } from '../src/save/PrinterBuilder';
-import { Scene } from '../src/Scene';
-import path from 'path';
+import { SceneConfig } from '../../src';
+import { PrinterBuilder } from '../../src';
+import { Scene } from '../../src';
+import { Logger, LOG_LEVEL, LOG_MODULE_TYPE } from '../../src';
 
-const log4js = require("log4js");
-const logger = log4js.getLogger("ark");
+const logger = Logger.getLogger(LOG_MODULE_TYPE.TOOL, 'DumpJson');
+Logger.configure('', LOG_LEVEL.ERROR, LOG_LEVEL.INFO, false);
 
 let config = new SceneConfig();
-config.buildFromProjectDir(path.join(__dirname, "resources/save"));
+config.buildFromProjectDir("tests/resources/save");
 let scene = new Scene();
 scene.buildSceneFromProjectDir(config);
 scene.inferTypes();
@@ -31,5 +31,5 @@ let printer = new PrinterBuilder();
 for (let f of scene.getFiles()) {
     logger.info("Processing: " + f.getFilePath());
     printer.dumpToTs(f);
-    printer.dumpToJson(f);
+    // printer.dumpToJson(f); // TODO: ArkAliasTypeDefineStmt adapt
 }
