23ac150d0997f8b67d63bbcc4275ea3675ea8df8
sfoolish <lqi27@163.com>
2025-03-25 21:07:33 +0800

    mv DefUseChainTest.ts to samples dir
    Signed-off-by: sfoolish <lqi27@163.com>

--- tests/samples/DefUseChainTest.ts
+++ tests/samples/DefUseChainTest.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,42 +13,45 @@
  * limitations under the License.
  */
 
-import { SceneConfig } from '../src/Config';
-import { Scene } from '../src/Scene';
-import { DEFAULT_ARK_METHOD_NAME } from '../src';
+import { SceneConfig } from '../../src';
+import { Scene } from '../../src';
+import { DEFAULT_ARK_METHOD_NAME } from '../../src';
+import { Logger, LOG_LEVEL, LOG_MODULE_TYPE } from '../../src';
+
+const logger = Logger.getLogger(LOG_MODULE_TYPE.TOOL, 'DefUseChainTest');
+Logger.configure('', LOG_LEVEL.ERROR, LOG_LEVEL.INFO, false);
 
 export class Test {
     public buildScene(): Scene {
-        const config_path = "tests\\resources\\defUseChain\\defUseChain.json";
+        const prjDir = "tests/resources/defUseChain";
         let config: SceneConfig = new SceneConfig();
-        config.buildFromJson(config_path);
-        return new Scene(config);
+        config.buildFromProjectDir(prjDir);
+        let projectScene: Scene = new Scene();
+        projectScene.buildSceneFromProjectDir(config);
+        return projectScene;
     }
 
     public test() {
         let scene = this.buildScene();
         scene.inferTypes();
 
-        for (const arkFile of scene.arkFiles) {
+        for (const arkFile of scene.getFiles()) {
             for (const arkClass of arkFile.getClasses()) {
                 for (const arkMethod of arkClass.getMethods()) {
                     if (arkMethod.getName() == DEFAULT_ARK_METHOD_NAME) {
                         continue;
                     }
-                    console.log('*** arkMethod: ', arkMethod.getName());
+                    logger.info('*** arkMethod: ', arkMethod.getName());
 
-                    const cfg = arkMethod.getBody().getCfg();
+                    const cfg = arkMethod.getBody()!.getCfg();
                     cfg.buildDefUseChain();
                     for (const chain of cfg.getDefUseChains()){
-                        console.log("variable: "+chain.value.toString()+", def: "+chain.def.toString()+", use: "+chain.use.toString());
+                        logger.info("variable: "+chain.value.toString()+", def: "+chain.def.toString()+", use: "+chain.use.toString());
                     }
-                    
                 }
             }
         }
     }
-
-
 
     public testTypeInference(): void {
         let scene = this.buildScene();
