741a97e57ae00f1957016a0d6ae38de0e12bda8d
Ji Xuye <jixuye@huawei.com>
2025-05-10 16:21:35 +0800

    global ref link export local
    Signed-off-by: Ji Xuye <jixuye@huawei.com>

--- tests/resources/exports/test.ts
+++ tests/resources/exports/test.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Copyright (c) 2024-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,9 +14,9 @@
  */
 
 import * as t from './exportSample';
+import { arr } from './exportSample';
 import hilog from '@ohos.hilog';
 import file from '@ohos.base';
-import path from 'path';
 
 export function cc() {
     t.testing();
@@ -40,3 +40,4 @@ class test {
 let sTest = new test();
 export default sTest as test;
 
+arr[3] = 2;
